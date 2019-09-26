a9d729e523475169d1e675ac3492f95e68d96c19
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-09-26 18:37:14 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan6-KambingStatic/src/latihan6/Latihan6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan6;
+
+/**
+ *
+ *  @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF1
+ * NIM			: 10118008
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan biodata rockstar
+ */
+public class Latihan6 {
+
+	/**
+	 */
+	public static final String NAMA_KAMBING = "MIDUN";
+	public static void main(String[] args) {
+		Mamalia.jumlahKambing = 485000;
+		System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+		// TODO code application logic here
+	}
+	
+}
